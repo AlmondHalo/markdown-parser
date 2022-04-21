41749f0f26312762f9481b9a4c207e269050a62a
AlmondHalo <cmrice@ucsd.edu>
2022-04-21 09:45:11 -0700

    Random changes

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,28 @@
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void MarkdownParse() throws IOException{
+        ArrayList<String> returned;
+        ArrayList<String> something = new ArrayList<>();
+        something.add("complete");
+        something.add("and utter");
+        something.add("BULLSHIT");
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        returned = MarkdownParse.getLinks(content);
+        assertEquals(returned, MarkdownParse.getLinks(content));
     }
 }
